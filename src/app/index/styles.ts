b563877467d50fcbe1d47bfad5491a3e9f467f16
Vitor <vitorwerle78@gmail.com>
2025-02-15 16:53:17 -0300

    style

---
+++ src/app/index/styles.ts
@@ -0,0 +1,14 @@
+import { colors } from "@/app/styles/colors"
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+    color: colors.green[300],
+    fontSize: 30,
+    }
+})
